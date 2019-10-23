3ae44828c017871484f1c1127cc51f3286428f84
ArjunaKumxr <arjunak@outlook.com>
2019-10-23 19:38:47 +0100

    Change the upper bound for number generation to 10

--- app/src/main/java/com/example/ma18aak1/diceroller/DiceRoller.java
+++ app/src/main/java/com/example/ma18aak1/diceroller/DiceRoller.java
@@ -9,15 +9,43 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.Random;
 
 public class DiceRoller extends AppCompatActivity {
 
+    private TextView Yeet;
+    private EditText numberEntered;
+    private Button validate;
+    private Object numberToFind;
+    private int counter = 0;
+    private TextView base;
+    private TextView view;
+
+
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_roller);
+
+        Yeet = (TextView) findViewById(R.id.numberTextView);
+        numberEntered = (EditText) findViewById(R.id.userinput);
+        base = (TextView) findViewById(R.id.count);
+        view = (TextView) findViewById(R.id.counter);
+
+
+
+
+
+
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -57,11 +85,21 @@ public class DiceRoller extends AppCompatActivity {
         TextView tv = (TextView) this.findViewById(R.id.numberTextView);
 
         Random r = new Random();
-        int number = r.nextInt( 10 + 1);
+        int number = r.nextInt(6)+ 1;
+
 
         tv.setText(Integer.toString(number));
 
+        int n = Integer.parseInt(numberEntered.getText().toString());
+
+        if (n < 1 || n > 6) {
+            Toast.makeText(this, "Please Input a number between 1 - 6", Toast.LENGTH_SHORT).show();
+        } else if (n == number) {
+            Toast.makeText(this, "Congratulations, you guessed the right number!", Toast.LENGTH_SHORT).show();
+            counter = counter  +1;
+            base.setText(Integer.toString(counter));
 
 
+        }
     }
 }
